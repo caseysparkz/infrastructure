aa9f2335e8ff1eb18aa83681eeaeea121ff24399
caseysparkz <himself@caseyspar.kz>
2025-02-04 21:44:00 -0800

    refactor(caseyspar.kz): removed aws keys from config

--- caseyspar.kz/providers.tf
+++ caseyspar.kz/providers.tf
@@ -28,8 +28,6 @@ terraform {
 ## Providers ==================================================================
 provider "aws" { #                                                              AWS.
   region     = var.aws_region
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
 
   default_tags {
     tags = {
