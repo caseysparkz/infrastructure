92083c90fdc962584d549a2115f8bc893dac6923
caseysparkz <himself@caseyspar.kz>
2025-02-10 18:38:09 -0800

    chore: provider upgrades

--- modules/forward_zones/providers.tf
+++ modules/forward_zones/providers.tf
@@ -4,10 +4,12 @@
 
 ## Terraform ==================================================================
 terraform {
+  required_version = "~> 1.10.5"
+
   required_providers {
     cloudflare = {
       source  = "cloudflare/cloudflare"
-      version = "~> 4.33"
+      version = "~> 4.52.0"
     }
   }
 }
