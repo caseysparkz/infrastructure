054ad2d9e6631c7602affbd6ec6993d8b1093240
caseysparkz <himself@caseyspar.kz>
2025-03-17 16:12:04 -0700

    chore(hashicorp/aws): upgrade to 5.91.0

--- tfstate_backend/providers.tf
+++ tfstate_backend/providers.tf
@@ -3,7 +3,7 @@
 #
 
 terraform {
-  required_version = "~> 1.10.5"
+  required_version = ">= 1.10.5, < 2.0.0"
 
   backend "s3" {
     region       = "us-west-2"
@@ -16,7 +16,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.89.0"
+      version = "~> 5.91.0"
     }
   }
 }
