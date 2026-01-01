05c2e58bf48cc49c7bc7f84ced31ef798ebf07f0
caseysparkz <himself@caseysparkz.com>
2026-01-01 13:27:13 -0800

    style(com/caseysparkz): Reduced number of files
    * Moved manifests => main.tf
    * Moved tfvars => varible defaults

--- com/caseysparkz/main.tf
+++ com/caseysparkz/main.tf
@@ -17,7 +17,59 @@ locals {
     { key = "rua", value = "mailto:dmarc_rua@${var.root_domain}" },
     { key = "ruf", value = "mailto:dmarc_ruf@${var.root_domain}" },
   ]
-  dmarc_policy = join(";", [for item in local.dmarc_list : "${item.key}=${item.value}"]) # Parse local.dmarc_list
+  dmarc_policy       = join(";", [for item in local.dmarc_list : "${item.key}=${item.value}"]) # Parse local.dmarc_list
+  cloudflare_comment = "Terraform managed."
+  cloudflare_zone_id = data.cloudflare_zones.root_domain.result[0].id
+  cloudflare_zone_settings = {
+    always_use_https         = "on"
+    automatic_https_rewrites = "on"
+    http3                    = "on"
+    min_tls_version          = "1.2"
+    opportunistic_encryption = "on"
+    ssl                      = "flexible"
+    tls_1_3                  = "on"
+  }
+}
+
+# Data =========================================================================
+data "cloudflare_zones" "root_domain" { name = var.root_domain }
+
+# Resources ====================================================================
+# AWS::KMS ---------------------------------------------------------------------
+resource "aws_kms_key" "this" {
+  description             = "KMS key to encrypt domain artifacts/S3 bucket objects."
+  deletion_window_in_days = 30
+  tags                    = { Service = "kms" }
+}
+
+# Cloudflare -------------------------------------------------------------------
+resource "cloudflare_zone_setting" "root_zone" {
+  for_each   = local.cloudflare_zone_settings
+  zone_id    = local.cloudflare_zone_id
+  setting_id = each.key
+  value      = each.value
+}
+
+resource "cloudflare_dns_record" "txt" {
+  for_each = var.txt_records
+  zone_id  = local.cloudflare_zone_id
+  name     = each.value
+  content  = each.key
+  type     = "TXT"
+  ttl      = 1
+  proxied  = false
+  comment  = local.cloudflare_comment
+}
+
+resource "cloudflare_dns_record" "pka" {
+  for_each = var.pka_records
+  zone_id  = local.cloudflare_zone_id
+  name     = "${each.key}._pka"
+  content  = "v=pka1; fpr=${each.value}"
+  type     = "TXT"
+  ttl      = 1
+  proxied  = false
+  comment  = local.cloudflare_comment
 }
 
 # Modules ======================================================================
@@ -52,4 +104,17 @@ module "proton_home" {
     "protonmail2._domainkey" = "protonmail2.domainkey.d4gc64isfcsi5uij7rmm2nggww7zvww7zvmtyw5guqxefeghia2wq.domains.proton.ch."
     "protonmail3._domainkey" = "protonmail3.domainkey.d4gc64isfcsi5uij7rmm2nggww7zvww7zvmtyw5guqxefeghia2wq.domains.proton.ch."
   }
+}
+
+# Outputs ======================================================================
+output "aws_kms_key_id" {
+  description = "ID of the KMS key used to encrypt all domain artifacts."
+  value       = aws_kms_key.this.key_id
+  sensitive   = false
+}
+
+output "aws_kms_key_arn" {
+  description = "ARN of the KMS key used to encrypt all domain artifacts."
+  value       = aws_kms_key.this.arn
+  sensitive   = false
 }
--- com/caseysparkz/variables.tf
+++ com/caseysparkz/variables.tf
@@ -8,11 +8,6 @@ variable "aws_region" {
   description = "AWS region to deploy to."
   sensitive   = false
   default     = "us-west-2"
-
-  validation {
-    condition     = contains(["us-east-1", "us-west-2"], var.aws_region)
-    error_message = "AWS region not in ['us-east-1', 'us-west-2']."
-  }
 }
 
 # Cloudflare ===================================================================
@@ -20,35 +15,48 @@ variable "mx_servers" {
   type        = map(string)
   description = "MX servers for root domain. Syntax: {server: priority}."
   sensitive   = false
-  default     = {}
+  default = {
+    "mail.protonmail.ch"    = 10
+    "mailsec.protonmail.ch" = 20
+  }
 }
 
 variable "dkim_records" {
   type        = map(string)
   description = "DKIM (CNAME) for root domain. Syntax: {host: pointer}."
   sensitive   = false
-  default     = {}
+  default = {
+    "protonmail._domainkey"  = "protonmail.domainkey.d56wvdqzbgjl657p6p37duzymskqqisyreca5lrft72j35tshomoq.domains.proton.ch"
+    "protonmail2._domainkey" = "protonmail2.domainkey.d56wvdqzbgjl657p6p37duzymskqqisyreca5lrft72j35tshomoq.domains.proton.ch"
+    "protonmail3._domainkey" = "protonmail3.domainkey.d56wvdqzbgjl657p6p37duzymskqqisyreca5lrft72j35tshomoq.domains.proton.ch"
+  }
 }
 
 variable "spf_senders" {
   type        = list(string)
   description = "List of allowed SPF senders, like: [\"include:_spf.example.com\", \"ip4:127.0.0.1\"]."
   sensitive   = false
-  default     = []
+  default = [
+    "include:_spf.protonmail.ch",
+    "mx"
+  ]
 }
 
 variable "txt_records" {
   type        = map(string)
   description = "List of TXT records for domain."
   sensitive   = false
-  default     = {}
+  default = {
+    "did=did:plc:eop37ikcn6s33dedyhvejqv5"                                  = "_atproto"
+    "keybase-site-verification=tlIvxzz3OeL0u3nDrGVYrXRlNX0o62Xm0daTHOfLTQI" = "@"
+  }
 }
 
 variable "pka_records" {
   type        = map(string)
   description = "Map of PKA handles and fingerprints for root domain."
   sensitive   = false
-  default     = {}
+  default     = { himself = "133898B4C51BC39479E97F1B2027DEDFECE6A3D5" }
 }
 
 # Misc. ========================================================================
