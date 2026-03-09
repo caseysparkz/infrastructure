203d75d3fba7297b2e996a52128618ed290f1ae4
caseysparkz <himself@caseysparkz.com>
2026-03-08 23:01:31 -0700

    chore(tfstate): hardened config
    Ran tfsec against manifest.
    
    * Enabled logging.
    * Enabled key rotation.
    * Enabled bucket versioning.
    * Enabled bucket logging.
    
    Moved resources in state.

--- tfstate/main.tf
+++ tfstate/main.tf
@@ -32,52 +32,62 @@ resource "aws_resourcegroups_group" "this" {
   }
 }
 
-resource "aws_kms_key" "terraform_state" {
+resource "aws_kms_key" "this" {
   description             = "KMS key for terraform state S3 bucket objects."
+  enable_key_rotation     = true
   deletion_window_in_days = 30
   tags                    = { Name = "${local.namespace}-kms-key" }
 }
 
-resource "aws_kms_alias" "terraform_state" {
+resource "aws_kms_alias" "this" {
   name          = "alias/${replace(var.bucket_name, ".", "/")}"
-  target_key_id = aws_kms_key.terraform_state.key_id
+  target_key_id = aws_kms_key.this.key_id
 }
 
-resource "aws_s3_bucket" "terraform_state" {
+resource "aws_s3_bucket" "this" {
   bucket        = var.bucket_name
   force_destroy = false
-  tags = {
-    Name    = "${local.namespace}-s3-state"
-    Service = "s3"
-  }
+  tags          = { Name = "${local.namespace}-s3-state" }
 }
 
-resource "aws_s3_bucket_ownership_controls" "terraform_state" {
-  bucket = aws_s3_bucket.terraform_state.id
+resource "aws_s3_bucket_versioning" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  versioning_configuration { status = "Enabled" }
+}
+
+resource "aws_s3_bucket_logging" "this" {
+  bucket        = aws_s3_bucket.this.id
+  target_bucket = "prod-caseysparkz-root-s3-bucket-logging"
+  target_prefix = "tfstate"
+}
+
+resource "aws_s3_bucket_ownership_controls" "this" {
+  bucket = aws_s3_bucket.this.id
 
   rule { object_ownership = "BucketOwnerPreferred" }
 }
 
-resource "aws_s3_bucket_acl" "terraform_state" {
-  depends_on = [aws_s3_bucket_ownership_controls.terraform_state]
-  bucket     = aws_s3_bucket.terraform_state.id
+resource "aws_s3_bucket_acl" "this" {
+  depends_on = [aws_s3_bucket_ownership_controls.this]
+  bucket     = aws_s3_bucket.this.id
   acl        = "private"
 }
 
-resource "aws_s3_bucket_public_access_block" "terraform_state" {
-  bucket                  = aws_s3_bucket.terraform_state.id
+resource "aws_s3_bucket_public_access_block" "this" {
+  bucket                  = aws_s3_bucket.this.id
   block_public_acls       = true
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
 
-resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" {
-  bucket = aws_s3_bucket.terraform_state.id
+resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
+  bucket = aws_s3_bucket.this.id
 
   rule {
     apply_server_side_encryption_by_default {
-      kms_master_key_id = aws_kms_key.terraform_state.key_id
+      kms_master_key_id = aws_kms_key.this.key_id
       sse_algorithm     = "aws:kms"
     }
   }
@@ -86,10 +96,10 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" 
 # Outputs ======================================================================
 output "aws_s3_bucket_name" {
   description = "FQDN of the S3 bucket (as expected by the Terraform backend config)."
-  value       = aws_s3_bucket.terraform_state.id
+  value       = aws_s3_bucket.this.id
 }
 
 output "aws_s3_bucket_uri" {
   description = "URI of the S3 bucket (as expected by the AWS CLI)."
-  value       = "s3://${aws_s3_bucket.terraform_state.id}/"
+  value       = "s3://${aws_s3_bucket.this.id}/"
 }
