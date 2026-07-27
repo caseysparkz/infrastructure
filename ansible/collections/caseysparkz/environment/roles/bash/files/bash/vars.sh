e8d71815b770161c6dfa6034427376068cd51968
caseysparkz <himself@caseysparkz.com>
2026-07-26 22:01:16 -0700

    feat(ansible/collections/caseysparkz/environment/roles/bash): updated variables

--- ansible/collections/caseysparkz/environment/roles/bash/files/bash/vars.sh
+++ ansible/collections/caseysparkz/environment/roles/bash/files/bash/vars.sh
@@ -38,13 +38,17 @@ export VARS="${HOME}/.bash/vars.sh"
 export ANSIBLE_DUPLICATE_YAML_DICT_KEY='ignore'
 
 # Dagger
-DAGGER_ENGINE_POD_NAME="$(kubectl get pod                                   \
-    --namespace=dagger                                                      \
-    --selector=name=dagger-dagger-helm-engine                               \
-    --output=jsonpath='{.items[0].metadata.name}'                           \
-)"
+if kubectl cluster-info > /dev/null 2>&1; then
+    if kubectl get pod --namespace=dagger --selector=name=dagger-dagger-helm-engine > /dev/null 2>&1; then
+        DAGGER_ENGINE_POD_NAME="$(kubectl get pod                                   \
+            --namespace=dagger                                                      \
+            --selector=name=dagger-dagger-helm-engine                               \
+            --output=jsonpath='{.items[0].metadata.name}'                           \
+        )"
+        export _EXPERIMENTAL_DAGGER_RUNNER_HOST="kube-pod://${DAGGER_ENGINE_POD_NAME}?namespace=dagger"
+    fi
+fi
 export DAGGER_NO_NAG='1'
-export _EXPERIMENTAL_DAGGER_RUNNER_HOST="kube-pod://${DAGGER_ENGINE_POD_NAME}?namespace=dagger"
 
 ## Docker
 export DOCKER_BUILDKIT='1'
