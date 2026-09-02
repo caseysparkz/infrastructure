116766e956af07d807ccebd652597b8ce07def19
caseysparkz <himself@caseysparkz.com>
2026-09-02 01:03:46 -0700

    feat(ansible/collections/caseysparkz/environment/roles/bash): updated secrets, vars

--- ansible/collections/caseysparkz/environment/roles/bash/files/bash/vars.sh
+++ ansible/collections/caseysparkz/environment/roles/bash/files/bash/vars.sh
@@ -23,6 +23,7 @@ export EDITOR='/usr/bin/vim'
 # Misc.
 export ALIASES="${HOME}/.bash_aliases"
 export DO_NOT_TRACK='1'
+export ECR='ecr.caseysparkz.com'
 export FUNCTIONS="${HOME}/.bash/functions"
 export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
 export GH='git@github.com'
